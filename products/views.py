c797acad56c183762a7255927c6d0fd92811cf98
AledPeart <aledpeart@gmail.com>
2021-11-17 11:53:51 +0000

    Added category filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages 
 from django.db.models import Q #allows us to serch with 'or' logic as well as 'and'
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,8 +10,17 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None #this is to ensure that error not thrown if search query=blank
+    categories = None
 
-    if request.GET: #GEWT method passed in to the url
+    if request.GET: #GET method passed in to the url
+
+        #NB __ syntax allows us to look for the 'name' field in the category model
+
+        if 'category' in request.GET: 
+            categories = request.GET['category'].split(',') #splits the categories into a list at the commas
+            products = products.filter(category__name__in=categories) #filters all products to just those in the list
+            categories = Category.objects.filter(name__in=categories) #filters all categories to those searched by user
+
         if 'q' in request.GET: #q is the name of our text input
             query = request.GET['q']
             if not query: #if a blank query submitterd
@@ -22,10 +31,11 @@ def all_products(request):
             #Q object where name OR description is in the query (|=or, i=case insensitive)
             products = products.filter(queries) #filter method to filter the queries
 
-             
+           
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
